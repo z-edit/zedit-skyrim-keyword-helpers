cf0d3a937b45cc8d4fa7861217d0bf7851c954e2
Colin <why101@msn.com>
2018-06-10 12:27:50 -0700

    added tests for skyrimMaterialService for armors and getMaterialKeyword

--- tests/testSkyrimMaterialService.js
+++ tests/testSkyrimMaterialService.js
@@ -2,8 +2,31 @@
 let {skyrimMaterialService} = zedit,
     log = console.log;
 
-// TEST WEAPONS
-log('== Testing Weapon Materials ==');
+log('== Testing skyrimMaterialService ==');
+
+// HELPER FUNCTIONS
+let logReducedObj = function(records, getKey) {
+    let obj = {};
+    records.forEach(rec => {
+        let key = getKey(rec);
+        if (!obj.hasOwnProperty(key)) obj[key] = [];
+        obj[key].push(xelib.LongName(rec));
+    });
+
+    log(JSON.stringify(obj, null, 2));
+};
+
+let getMaterialStr = rec => {
+    return skyrimMaterialService.getMaterial(rec) + '';
+};
+
+let getMaterialKeywordValue = rec => {
+    let keyword = skyrimMaterialService.getMaterialKeyword(rec);
+    return keyword ? xelib.GetValue(keyword) : 'undefined';
+};
+
+// LOAD WEAPONS
+log('Loading weapon records...');
 let weapons = xelib.GetRecords(0, 'WEAP').filter(rec => {
     return xelib.HasElement(rec, 'FULL') &&
         !xelib.HasElement(rec, 'CNAM') &&
@@ -14,12 +37,24 @@ let weapons = xelib.GetRecords(0, 'WEAP').filter(rec => {
 
 log(`${weapons.length} weapons found.`);
 
-// test getMaterial
-let obj = {};
-weapons.forEach(rec => {
-    let material = skyrimMaterialService.getMaterial(rec) + '';
-    if (!obj.hasOwnProperty(material)) obj[material] = [];
-    obj[material].push(xelib.LongName(rec));
+// LOAD ARMORS
+log('Loading armor records...');
+let armors = xelib.GetRecords(0, 'ARMO').filter(rec => {
+    return xelib.HasElement(rec, 'FULL') &&
+        !xelib.HasElement(rec, 'CNAM') &&
+        !xelib.HasElement(rec, 'EITM') &&
+        xelib.HasElement(rec, 'KWDA');
 });
 
-log(JSON.stringify(obj, null, 2));
+log(`${armors.length} armors found.`);
+
+// test getMaterial
+logReducedObj(weapons, getMaterialStr);
+logReducedObj(armors, getMaterialStr);
+
+// test getMaterialKeyword
+logReducedObj(weapons, getMaterialKeywordValue);
+logReducedObj(armors, getMaterialKeywordValue);
+
+// test setMaterialKeyword
+// TODO
